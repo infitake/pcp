11b3d70946cdd819f2d00814f1707a638f315570
infitake <rvndryadv@gmail.com>
2020-11-19 21:17:16 +0530

    new code

---
+++ codeforces/667a.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 100005
+#define MOD 1000000007
+#define dd double
+#define vec vector<ll>
+#define pii pair<ll,ll>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define rep1(i,b) for(int i=1;i<=b;i++)
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define clr(x) x.clear()
+#define sz(x) ((int)(x).size())
+#define F first
+#define S second
+
+int main() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	//ios_base& scientific (ios_base& str);
+	ll t,n,a,b; cin>>t;
+	while(t--){
+		cin>>a>>b;
+		n=abs(a-b);
+		ll k=10,count=0;
+
+		while(n!=0){
+			if(n>=k) {
+				count += n/k;
+				n=n%k;
+			}
+			else{
+				while(n!=k) k--;
+			}
+		}
+		cout<<count<<endl;
+	}
+	return 0;
+}
---
+++ codeforces/98a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 100005
+#define MOD 1000000007
+#define dd double
+#define vec vector<ll>
+#define pii pair<ll,ll>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define rep1(i,b) for(int i=1;i<=b;i++)
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define clr(x) x.clear()
+#define sz(x) ((int)(x).size())
+#define F first
+#define S second
+
+int main() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	//ios_base& scientific (ios_base& str);
+	ll t,n,a,b; cin>>t;
+	while(t--){
+		cin>>a>>b;
+		n = max(a,b);
+		if(a==b) cout<<2*a<<endl;
+		else cout<<2*n -1 <<endl;
+	}
+	return 0;
+}
